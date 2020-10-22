a501f94699bd3bb2d20472f624f9cd7ccca248f1
Patrick Wardle <patrick@objective-see.com>
2020-10-21 20:59:33 -1000

    dmg build, project cleanup
    files related to .dmg build
    project cleanup

---
+++ DMG/createDMG.sh
@@ -0,0 +1,12 @@
+create-dmg \
+  --volname "LuLu v2.0.0" \
+  --volicon "LuLu.icns" \
+  --background "background.png" \
+  --window-pos 200 120 \
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "LuLu.app" 200 190 \
+  --hide-extension "LuLu.app" \
+  --app-drop-link 600 185 \
+  "LuLu_2.0.0.dmg" \
+  "Release/"
